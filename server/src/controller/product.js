bdcea527d24d5eeaf49673673679214ef61f7dd8
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2022-07-09 18:32:20 +0700

    get products

--- server/src/controller/product.js
+++ server/src/controller/product.js
@@ -23,6 +23,73 @@ const getPromotion = async (req, res) =>{
         data:rows
     })
 }
+
+const getProducts = async (req, res) =>{
+    const offset = req.query.offset;
+    const size = req.query.size;
+    const [row_products, fields_products] = await pool.execute(`SELECT * FROM product 
+    limit ${offset}, ${size}`);
+
+    const [row_images, fields_images] =  await pool.execute(`SELECT image.* FROM image, product
+    INNER JOIN 
+	(select id_product 
+            	from product
+        		limit ${offset}, ${size}
+	) as product2
+ON product.id_product = product2.id_product 
+    WHERE image.id_product = product.id_product 
+    `);
+
+    const [row_colors, fields_colors] =  await pool.execute(`SELECT color.* FROM color, product
+    INNER JOIN 
+	(select id_product 
+            	from product
+        		limit ${offset}, ${size}
+	) as product2
+    ON product.id_product = product2.id_product 
+    WHERE color.id_product = product.id_product
+    `);
+
+    const [row_promotions, fields_promotion] = await pool.execute(`SELECT product.id_product, product.price as price_root, promotion.percent, (product.price * (1 - promotion.percent/100) ) as price_discount  FROM 	   product , promotion
+	WHERE promotion.id_promotion = product.id_promotion
+    limit ${offset}, ${size}`);
+
+    row_products.forEach((product)=>{
+        const formatter = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'VND',
+            minimumFractionDigits: 0,
+        });
+        product.price = formatter.format(product.price);
+
+        row_images.forEach((image)=>{
+            if(product.id_product === image.id_product){
+               if(!Array.isArray(product.images)) {
+                product.images = [];
+                }  
+                image.image_link = process.env.URL +"/images/products/" +image.image_link ;
+                product.images.push(image)
+            }
+        });
+        row_colors.forEach((color)=>{
+            if(!Array.isArray(product.colors)) {
+                product.colors = [];
+                }  
+                product.colors.push(color)
+        });
+        row_promotions.forEach((promotion)=>{
+            if(promotion.id_product === product.id_product){
+                product.price_discount = formatter.format(promotion.price_discount);
+                product.percent = promotion.percent;
+            }
+        })
+    })
+
+    return res.status(200).json({
+        data:row_products
+    })
+}
 export {
-    getPromotion
+    getPromotion,
+    getProducts
 }
--- server/src/services/productApi.js
+++ server/src/services/productApi.js
@@ -1,5 +1,6 @@
 import * as productController from '../controller/product';
 function productApi(router){
-    router.get('/product/promotion/',productController.getPromotion)
+    router.get('/product/promotion/',productController.getPromotion);
+    router.get('/product/limit',productController.getProducts);
 }
 export default productApi
