7e899caf7580ee8808ac818477e0004247d86c15
vanthienpham <vanthien.dev@gmail.com>
2022-11-10 21:09:17 +0700

    scroll top when route change

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import {BrowserRouter,Route, Routes} from 'react-router-dom'
 import {publicRoutes, privateRoutes} from '~/routes'
+import ScrollToTop from './helpers/ScrollToTop';
 
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter >
+      <ScrollToTop />
       <div className="App">
         {
-          <Routes>
+          <Routes  >
             {
               publicRoutes.map((route, index) =>{
                 let Layout = route.layout;
                 const Page = route.component;
-                return <Route key={index} path={route.path} element={<Layout>{ <Page /> }</Layout> } />
+                return <Route 
+                 
+                  key={index} path={route.path} element={<Layout>{ <Page /> }</Layout> } />
               })
             }
           
